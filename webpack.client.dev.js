6a73fe76a3a041e01b003791476db381d45fedd8
nlaffey <nick.laffey@gmail.com>
2018-07-29 11:53:51 -0400

    misc

--- webpack.client.dev.js
+++ webpack.client.dev.js
@@ -5,7 +5,7 @@ const LiveReloadPlugin = require('webpack-livereload-plugin');
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 // const bundleAnalyzer = new BundleAnalyzerPlugin();
 
-const liveReload = new LiveReloadPlugin({ appendScriptTag: true });
+const liveReload = new LiveReloadPlugin({ appendScriptTag: true, delay: 1000 });
 
 module.exports = merge(clientCommon, {
   plugins: [liveReload],
