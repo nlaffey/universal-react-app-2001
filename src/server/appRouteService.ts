f28f517d15c6865457799222ae89b686f477868c
nlaffey <nick.laffey@gmail.com>
2018-08-08 20:03:18 -0400

    Refactoring universal route handler

--- src/server/appRouteService.ts
+++ src/server/appRouteService.ts
@@ -23,27 +23,22 @@ const createAppRouteHandlers: CreateAppRouteHandlers =
       const { query, pathname } = parsedUrl;
       const context = { insertCss };
       const resolveObject: ResolveObject = { pathname, query, context };
-      universalRouter.resolve(resolveObject).then(async (component) => {
-        try {
-          const initialPropsContext: InitialPropsContext = { port, resolveObject };
-          const initialProps = await getInitialProps(component, initialPropsContext);
-          const resolveObjectWithProps = { ...resolveObject, initialProps };
-          universalRouter.resolve(resolveObjectWithProps).then((componentWithProps) => {
-            const componentHtml = renderToString(componentWithProps);
-            const css = getRouteCss();
-            const html = renderIndexHtmlTemplate(componentHtml, FULL_BUNDLE_URL, initialProps, css);
-            res.send(html);
-          });
-        } catch (err) {
-          // TODO: Handle this more gracefully in production
-          res.send(err.stack);
-        }
-      }).catch((err) => {
+      try {
+        const component = await universalRouter.resolve(resolveObject);
+        const initialPropsContext: InitialPropsContext = { port, resolveObject };
+        const initialProps = await getInitialProps(component, initialPropsContext);
+        const resolveObjectWithProps = { ...resolveObject, initialProps };
+        const componentWithProps = await universalRouter.resolve(resolveObjectWithProps);
+        const componentHtml = renderToString(componentWithProps);
+        const css = getRouteCss();
+        const html = renderIndexHtmlTemplate(componentHtml, FULL_BUNDLE_URL, initialProps, css);
+        res.send(html);
+      } catch (err) {
         if (err.status !== 404) {
           console.error(err);
         }
         next();
-      });
+      }
     }
 
     async function handleContentfulGetRequest(req: Request, res: Response) {
